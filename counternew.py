c7e11f9898eda1c951b0af8ceafcc79b01e2458c
mehmetzantur <mehmetzantur@gmail.com>
2019-08-06 16:20:42 +0300

    stabil 1

--- counternew.py
+++ counternew.py
@@ -15,36 +15,78 @@ def main():
 	sigtick = False
 	isFirst = True
 	
+	status = 0
 	counterx = 1
 	
 	GPIO.cleanup()
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	
-	#GPIO.setup(11, GPIO.IN)
+	GPIO.setup(13, GPIO.OUT)
+	GPIO.setup(15, GPIO.IN)
+	
 	print('Pulse listening is started...')
 
 	
 	while 1:
-
-		if GPIO.input(11) and counterx == 1:
-
+		
+		
+		if GPIO.input(11) == 1 and counterx == 1:
+			counterx = 0
 			counter = counter + 1
 			print('pulse ' + str(counter))
 			
 			while 1:
 				
-				if GPIO.input(11) == True and counterx == 1:
-					counterx = 2
+				if GPIO.input(11) == 1 and counterx == 1:
+					counterx = 0
 					pass
 				
-				if GPIO.input(11) == False and counterx == 2:
+				if GPIO.input(11) == 0 and counterx == 0:
 					counterx = 1
-					break
+					break		
+			time.sleep(0.1)
+
+		# if GPIO.input(11) == 1:
+			# if status == 0:
+						
+				# GPIO.output(13, 1)
+				
+				# if GPIO.input(15) == 1:
+					# GPIO.output(13, 0)
+					# counter = counter + 1
+					# print('pulse ' + str(counter))
+					# status = 1
+					
+					# while 1:
+						# if GPIO.input(11) == 1:
+							# GPIO.output(13, 0)
+							# pass
+						# else:
+							# status = 0
+							# break
+				
+			# else:
+				# status = 0
 					
 					
 					
-					
+	
+	
+# if GPIO.input(11) and counterx == 1:
+
+	# counter = counter + 1
+	# print('pulse ' + str(counter))
+	
+	# while 1:
+		
+		# if GPIO.input(11) == True and counterx == 1:
+			# counterx = 2
+			# pass
+		
+		# if GPIO.input(11) == False and counterx == 2:
+			# counterx = 1
+			# break				
 					
 					
 if __name__ == '__main__':
